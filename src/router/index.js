0328f7618161325b042a48dfd551b902b27ff6e0
Vlad Petlyuk <vlad10112005@inbox.ru>
2023-12-15 15:03:08 +0300

    Made routes protection

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
+import firebase from "firebase/compat/app";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("@/views/Home.vue"),
   },
   {
@@ -16,37 +17,37 @@ const routes = [
   {
     path: "/categories",
     name: "categories",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("@/views/Categories.vue"),
   },
   {
     path: "/detail-record",
     name: "detail-record",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("@/views/DetailRecord.vue"),
   },
   {
     path: "/history",
     name: "history",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("@/views/History.vue"),
   },
   {
     path: "/planning",
     name: "planning",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("@/views/Planning.vue"),
   },
   {
     path: "/profile",
     name: "profile",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("@/views/Profile.vue"),
   },
   {
     path: "/record",
     name: "record",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("@/views/Record.vue"),
   },
   {
@@ -60,6 +61,16 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requireAuth = to.matched.some((record) => record.meta.auth);
+  if (requireAuth && !currentUser) {
+    next("/login?message=login");
+  } else {
+    next();
+  }
 });
 
 export default router;
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,6 @@
 export default {
   logout: "Вы вышли из системы",
+  login: "Для начала зайдите в систему",
   "auth/invalid-login-credentials": "Пользователя с таким email не существует",
   "auth/email-already-in-use": "email уже используется",
 };
